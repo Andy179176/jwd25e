4619b2719afe3ad91a51e40955c3b9d1d89411b5
andy179176 <andy179176@gmail.com>
2022-05-25 23:04:53 +0200

    20220518

---
+++ 20220518_2/src/com/company/MyClass.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class MyClass {
+
+    public boolean isYearLeap(int year){
+        return (year%4==0) && ((year%100!=0) || (year%400==0));
+    }
+
+}
--- MailService/src/com/company/DHL.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class DHL implements MailDeliveryService {
-
-    @Override
-    public String sendMail() {
-        return "Put a letter in an envelope, put a stamp, send";
-    }
-}
